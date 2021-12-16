660dd3d5442aab3bb49d0ee1be8aa70184dd3e4b
ASlava12 <vchslvartemenkov@gmail.com>
2021-12-16 03:29:02 +0300

    lesson 14 done (#14)

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -1,11 +1,7 @@
-from basketapp.models import Basket
-
-
 def basket(request):
     basket = []
-
     if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+        basket = request.user.basket.select_related()
 
     return {
         "basket": basket,
