4c6e7279fb244272c4899e3b87d07da4180a1e4f
root <root@django.nas.su>
2021-12-19 23:47:46 +0000

    Deploy fix

--- .docker/app.Dockerfile
+++ .docker/app.Dockerfile
@@ -10,6 +10,9 @@ RUN apt update && apt upgrade -y && apt-get purge && \
     useradd -g www-data -m django && \
     # chown -R django:www-data /geekshop && \
     # chmod -R 744 /geekshop &&\
+    mkdir -p /var/run/geekshop/ &&\
+    chown -R django:www-data /var/run/geekshop/ &&\
+    chmod -R 744 /var/run/geekshop/ &&\
     apt-get clean && apt-get autoclean
 
 CMD ["/geekshop/init_app.sh"]
--- init_app.sh
+++ init_app.sh
@@ -5,4 +5,4 @@ export DJANGO_PRODUCTION=1;
 python ./manage.py migrate;
 python ./manage.py collectstatic --noinput;
 
-echo "gunicorn --access-logfile - --env DJANGO_PRODUCTION=1 --bind unix:/geekshop/geekshop.sock --workers 3 geekshop.wsgi:application" | su django -G www-data 
+echo "gunicorn --access-logfile - --env DJANGO_PRODUCTION=1 --bind unix:/var/run/geekshop/geekshop.sock --workers 3 geekshop.wsgi:application" | su django -G www-data 
